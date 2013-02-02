97ba7dc124c699825e32cc4f2682fc5776258490
alvarporcar <alvar.porcar@gmail.com>
2013-02-02 09:34:36 +0100

    El meu segon commit

--- exemple1/urls.py
+++ exemple1/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from exemple1.views import current_datetime
-from exemple1.books.views import search
+from exemple1.books.views import search, contact
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,15 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^time/$',current_datetime),
     (r'^search/$', search),
-    
-    # Examples:
-    # url(r'^$', 'exemple1.views.home', name='home'),
-    
-    # url(r'^exemple1/', include('exemple1.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^contact/$', contact),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
