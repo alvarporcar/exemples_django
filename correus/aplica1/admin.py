4d679030edfa3bc2d94ab8b99d94695359811c36
alvarporcar <alvar.porcar@gmail.com>
2013-02-19 20:30:24 +0100

    Refinant el model de dades

--- correus/aplica1/admin.py
+++ correus/aplica1/admin.py
@@ -1,13 +1,25 @@
-from aplica1.models import Usuaris, Organismes, Projectes, Carrecs, Tipus_Organismes, Usuaris_Projectes
+from aplica1.models import Usuaris, Organismes, Projectes, Carrecs, Tipus_Organismes
 from django.contrib import admin
 
-admin.site.register(Usuaris)
-admin.site.register(Organismes)
-admin.site.register(Projectes)
-admin.site.register(Carrecs)
-admin.site.register(Tipus_Organismes)
-admin.site.register(Usuaris_Projectes)
 
 class UsuarisAdmin(admin.ModelAdmin):
-    list_display= ('__unicode__','cognoms','organisme')
+    list_display= ('cognoms','nom', 'email1','email2','tel1','carrec','organisme','notes')
+    
+    list_filter= ('organisme','projectes','carrec')
+    search_fields = ['cognoms']
+
+class OrganismesAdmin(admin.ModelAdmin):
+    list_display= ('nom', 'email1','tel1','fax1','web','notes')
+    search_fields = ['nom']
+
+class ProjectesAdmin(admin.ModelAdmin):
+    list_display = ('nom','abrev','web', 'responsable','notes')
+    search_fields = ['nom']
+
+admin.site.register(Usuaris, UsuarisAdmin)
+admin.site.register(Organismes,OrganismesAdmin)
+admin.site.register(Projectes,ProjectesAdmin)
+admin.site.register(Carrecs)
+admin.site.register(Tipus_Organismes)
+
 
